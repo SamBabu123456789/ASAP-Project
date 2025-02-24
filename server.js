430d4eb5696b794b91996bbb58154d57d9e67b04
SamBabu123456789 <sambabupolimetla@gmail.com>
2025-02-24 10:40:32 +0530

    done

---
+++ server.js
@@ -0,0 +1,37 @@
+const express =require('express')
+const mongoose = require('mongoose');
+const PORT = 4700
+const app = express()
+
+
+const port = process.env.PORT || 3000;
+
+
+const mongoURI = `mongodb+srv://sambabupolimetla:root@cluster0.xxyl1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
+
+let dbConnected = false;
+
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected successfully!');
+    dbConnected = true; 
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    dbConnected = false; 
+  });
+
+
+app.get('/', (req, res) => {
+  if (dbConnected) {
+    res.send('MongoDB connected successfully!');
+  } else {
+    res.status(500).send('MongoDB connection failed!');
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ userschema.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+const user =new mongoose.schema({
+    name:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    roles:{type:String,default:[]},
+
+})
+
+module.exports =  mongoose.model('user',userSchema)
+
+
+const profileschema =new mongoose.Schema({
+    firstName:{type:String},
+    lastName:{type:String},
+    age:{type:Number},
+})
+const profile = mongoose.model('profile',mongoose.schema)
+const activitySchema =  new mongoose.schema({
+    lastlogin:{type:Date}
+})
+const activity =mongoose.model('activity',activityschema)
